1129e95966f3823231a7c090edcbd29618f4d562
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-03-06 19:29:16 +0530

    created student class

---
+++ student.rb
@@ -0,0 +1,19 @@
+require './person'
+
+class Student < Person
+  attr_reader :classroom
+
+  def initialize(classroom, age, name, parent_permission)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
+end
