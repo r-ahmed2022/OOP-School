18f6ef34343e1e99d14275c200a4f056155d403d
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-03-07 19:10:18 +0530

    fixed trimmer_decorator

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,6 +2,6 @@ require './decorator'
 
 class TrimmerDecorator < Decorator
   def correct_name
-    @nameable.correct_name[1...10] if @nameable.correct_name.length > 10
+    @nameable.correct_name[0...10] ? @nameable.correct_name.length > 10 : @nameable.correct_name
   end
 end
