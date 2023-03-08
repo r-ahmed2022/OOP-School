e5c63dd8dd9fff761a950fc705530fc0817a78a6
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-03-08 17:51:13 +0530

    updated rentals

--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ require './capitalize_decorator'
 require './trimmer_decorator'
 
 class Person < Nameable
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals,  
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
