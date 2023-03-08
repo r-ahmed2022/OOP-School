562816e60ae3adb47313061f7faf85211c3210f2
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-03-08 17:52:36 +0530

    fixed linters

--- student.rb
+++ student.rb
@@ -11,6 +11,7 @@ class Student < Person
   def play_hooky
     '¯(ツ)/¯'
   end
+
   def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
