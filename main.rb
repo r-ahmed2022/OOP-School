e2c896548236ec0a223d011e9a62dc2c82b73292
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-03-11 18:45:16 +0530

    added bug

--- main.rb
+++ main.rb
@@ -1,11 +1,6 @@
-require './app'
-require './displaymenu'
+
 
 def main
-  menu = DisplayMenu.new
-  library = App.new
-  puts "\nWelcome to School Library!\n\n"
-  menu.display_menu(library)
-end
+
 
 main
