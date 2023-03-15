aa2d99492f36083999369f25b22d9ed9b12752c0
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-03-15 12:33:57 +0530

    linters

--- load.rb
+++ load.rb
@@ -3,6 +3,7 @@ require 'json'
 
 def load_data(library)
   load_books(library)
+  load_people(library)
 end
 
 def load_books(library)
@@ -11,5 +12,19 @@ def load_books(library)
   books.each do |book|
     book = Book.new(book['title'], book['author'])
     library.books.push(book)
+  end
+end
+
+def load_people(library)
+  File.write('people.json', JSON.generate([])) unless File.exist?('people.json')
+  people = JSON.parse(File.read('people.json'))
+  people.each do |person|
+    if person['type'] == 'Student'
+      student = Student.new('Unknown', person['age'], person['name'], parent_permission: person['permission'])
+      library.people.push(student)
+    else
+      teacher = Teacher.new(person['specialization'], person['age'], person['name'])
+      library.people.push(teacher)
+    end
   end
 end
--- store.rb
+++ store.rb
@@ -2,6 +2,7 @@ require 'json'
 
 def store_data(library)
   store_books(library)
+  store_people(library)
 end
 
 def store_books(library)
@@ -11,4 +12,18 @@ def store_books(library)
     books.push(book)
   end
   File.write('books.json', "#{JSON.generate(books)}\n")
+end
+
+def store_people(library)
+  people = []
+  library.people.each do |person|
+    if person.class.to_s == 'Student'
+      student = { name: person.name, age: person.age, permission: person.parent_permission, type: person.class }
+      people.push(student)
+    else
+      teacher = { name: person.name, age: person.age, specialization: person.specialization, type: person.class }
+      people.push(teacher)
+    end
+  end
+  File.write('people.json', "#{JSON.generate(people)}\n")
 end
