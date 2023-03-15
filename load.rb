b198a024374ca07c20ef454b38e2b0ddd5a76441
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-03-15 11:47:33 +0530

    linters

--- load.rb
+++ load.rb
@@ -2,14 +2,14 @@ require 'json'
 
 
 def load_data(library)
-    load_books(library)
-  end
+  load_books(library)
+end
 
 def load_books(library)
-    File.write('books.json', JSON.generate([])) unless File.exist?('books.json')
-    books = JSON.parse(File.read('books.json'))
-    books.each do |book|
-      book = Book.new(book['title'], book['author'])
-      library.books.push(book)
-    end
+  File.write('books.json', JSON.generate([])) unless File.exist?('books.json')
+  books = JSON.parse(File.read('books.json'))
+  books.each do |book|
+    book = Book.new(book['title'], book['author'])
+    library.books.push(book)
   end
+end
